32ec92ac252800548dfe73d772d98842153a5db2
Chamke <terzimehic.amil@hotmail.com>
2022-04-01 15:17:24 +0200

    Added ex01,2,3,4,5,6

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+
+    var myFirstName = "Amil";
+    var myLastName = "Terzimehic";
+    var myHomeTown = "Maglaj";
+
+  return{
+      myFirstName,
+      myLastName,
+      myHomeTown
+  };
+}
+
+console.log(main());
+module.exports = main;
---
+++ ex04/src/index.js
@@ -0,0 +1,7 @@
+function main(){
+
+    var myString = "FirstLine\n\tSecondLine\nThirdline";
+    return myString;
+}
+console.log(main());
+module.exports = main;
